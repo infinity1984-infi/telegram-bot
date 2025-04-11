2d57997d05abf66e7400c8fda8a3f5d0d40c5390
infinity1984-infi <ind.agamer7@gmail.com>
2025-04-11 13:59:27 +0530

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
